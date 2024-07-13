7a24079d1102484b7ede99fabbd0d093207d7258
Anton Kurylo <antonkurilo9@gmail.com>
2024-07-13 19:35:04 +0300

    add Basic authentication scheme, a Role entity and set access to the endpoints based on the user's role

--- src/main/java/mate/academy/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/service/impl/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package mate.academy.service.impl;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.user.UserRegistrationRequestDto;
 import mate.academy.dto.user.UserResponseDto;
+import mate.academy.exception.EntityNotFoundException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.mapper.UserMapper;
+import mate.academy.model.Role;
+import mate.academy.model.User;
+import mate.academy.repository.role.RoleRepository;
 import mate.academy.repository.user.UserRepository;
 import mate.academy.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -22,8 +30,12 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException(
                     "The user with the email is already registered: " + requestDto.email());
         }
+        User user = userMapper.toEntity(requestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER).orElseThrow(() ->
+                new EntityNotFoundException("Can't find the USER role"));
+        user.setRoles(Set.of(userRole));
         return userMapper.toDto(
-                userRepository.save(
-                        userMapper.toEntity(requestDto)));
+                userRepository.save(user));
     }
 }
